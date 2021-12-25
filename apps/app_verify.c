95576cd41e01fbcdc6bd23fed8f1753079b70434
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2021-12-25 15:47:29 -0500

    app_verify: fix message fallthrough to success branch

--- apps/app_verify.c
+++ apps/app_verify.c
@@ -1258,10 +1258,10 @@ static int verify_exec(struct ast_channel *chan, const char *data)
 			}
 			goto fail;
 		}
-success: /* only as a branch, if we fall through to here, that doesn't necessarily mean success */
 		if (!success) {
-			ast_log(LOG_WARNING, "Hit the success branch, but success flag not set. Please report this bug.\n"); /* should never happen! */
+			goto fail;
 		}
+success: /* only as a branch, if we fall through to here, that doesn't necessarily mean success */
 		verify_set_var(chan, local_var, vresult);
 		ast_verb(3, "Verification result for %s is '%s' (SUCCESS)\n", name, vresult ? vresult : "(null)");
 		goto done;
